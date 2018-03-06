1e418aefbbcf456666d01cb3501a7504e494f97f
jartherogue <jasreev@gmail.com>
2018-03-05 20:59:54 -0700

    added firebase functionality

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var x = document.getElementsByTagName("body");
-
-
+var config = {
+    apiKey: "AIzaSyDlQ2kWC8Qw9S5xG-R2ZxMjv1BlWELtEiM",
+    authDomain: "bigclassproject-bc504.firebaseapp.com",
+    databaseURL: "https://bigclassproject-bc504.firebaseio.com",
+    storageBucket: "bigclassproject-bc504.appspot.com",
+  };
+  firebase.initializeApp(config);
+  var dataPoint = firebase.database();
+  var imageURL = "";
 var browserLatitude = 0;
 var browserLongitude = 0;
 function getLocation() {
@@ -42,17 +49,25 @@ $("#submit").on("click", function (event) {
         console.log(response.trails);
         for (let i = 0; i < response.trails.length; i++) {
             var trailLink = "<a href='"+ response.trails[i].url +"'>Trail Link</a>"
-
+            var imageURL = response.trails[i].imgSmallMed;
+            dataPoint.ref().push({
+                imageURL: imageURL
+            });
             var newRow = ("<tr>" +
             "<td>" + response.trails[i].name + "</td>" +
             "<td>" + response.trails[i].difficulty + "</td>" +
             "<td>" + response.trails[i].summary + "</td>" +
-            "<td>" + response.trails[i]. + trailLink + "</td>" +
+            "<td>" + response.trails[i].trailLink + "</td>" +
             "</tr>");
             $("#trailinfo").append(newRow);
         }
 
     });
-
-
+    dataPoint.ref().on("child_added", function(snapshot) {
+        console.log(snapshot.val().imageURL);
+        var hikeImg = $("<img>");
+        hikeImg.attr("src", snapshot.val().imageURL );
+        hikeImg.attr("alt", "Image from the Hike");
+        $('#hikeImage').html(hikeImg);
+    });
 });
